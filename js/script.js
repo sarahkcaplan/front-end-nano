186c11236483537a4457a6fb4ca6854cd9ad6ded
Sarah Caplan <caplan.sarah@gmail.com>
2017-08-31 19:39:14 -0700

    Add NYT articles about city to page

--- js/script.js
+++ js/script.js
@@ -23,29 +23,22 @@ function loadData() {
 
     // YOUR CODE GOES HERE!
 
-    $body.append('<img class="bgimg" src="' + streetviewUrl + '">');
+    $body.append('<img class="bgimg" src="' + streetviewURL + '">');
 
+    var nytimesUrl = 'http://api.nytimes.com/svc/search/v2/articlesearch.json?q='+ cityStr + '&sort=newest&api-key=81e6f6f0b0cd433cb106e2f4c02208db'
+    $.getJSON(nytimesUrl, function( data ) {
+
+      $nytHeaderElem.text('New York Times Articles About' + cityStr);
+      articles = data.response.docs;
+      for(var i = 0; i < articles.length; i++) {
+        var article = articles[i];
+        $nytElem.append('<li class = "article">' + '<a href = "'+article.web_url+'">'
+        + article.headline.main + '</a>' + '<p>' + article.snippet + '</p>' + '</li>');
+      };
+    }).error(function(e){
+      $nytHeaderElem.text('Page could not be loaded');
+    });
     return false;
-
-    // // Your NYTimes AJAX request goes here
-
-    // var nytURL = 'https://api.nytimes.com/svc/search/v2/articlesearch.json?q=' +
-    //     cityStr + '&sort=newest&api-key=81e6f6f0b0cd433cb106e2f4c02208db';
-
-    // $.getJSON(nytURL, function(data) {
-
-    //     $nytHeaderElem.text('New York Times Articles About '+ cityStr);
-
-    //     articles = data.response.docs;
-    //     for (var i = 0; i < articles.length; i++) {
-    //         var article = articles[i];
-    //         $nytElm.append('<li class="article">'+
-    //             '<a href="'+article.web_url+'">'+article.headline.main+
-    //                 '</a>'+
-    //                 '<p>' + article.snippet + '</p>'+
-    //             '</li>');
-    //     };
-    // });
 };
 
 $('#form-container').submit(loadData);
