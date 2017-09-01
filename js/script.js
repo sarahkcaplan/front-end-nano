74d91f24125dec3c2a78b33bc7cc3526f2b61aae
Sarah Caplan <caplan.sarah@gmail.com>
2017-08-31 19:07:02 -0700

    Add greeting to top of page listing address

--- js/script.js
+++ js/script.js
@@ -1,4 +1,3 @@
-
 function loadData() {
 
     var $body = $('body');
@@ -13,9 +12,40 @@ function loadData() {
 
     // load streetview
 
+
+    var streetStr = $('#street').val(); //'street' is input id
+    var cityStr = $('#city').val(); //'city' is input id
+    var address = streetStr+', '+cityStr;
+
+    $greeting.text('So, you want to live at ' + address + '?');
+
+    var streetviewURL = 'http://maps.googleapis.com/maps/api/streetview?size=600x400&location=' +address + '';
+
     // YOUR CODE GOES HERE!
 
+    $body.append('<img class="bgimg" src="' + streetviewURL + '">');
+
     return false;
+
+    // // Your NYTimes AJAX request goes here
+
+    // var nytURL = 'https://api.nytimes.com/svc/search/v2/articlesearch.json?q=' +
+    //     cityStr + '&sort=newest&api-key=81e6f6f0b0cd433cb106e2f4c02208db';
+
+    // $.getJSON(nytURL, function(data) {
+
+    //     $nytHeaderElem.text('New York Times Articles About '+ cityStr);
+
+    //     articles = data.response.docs;
+    //     for (var i = 0; i < articles.length; i++) {
+    //         var article = articles[i];
+    //         $nytElm.append('<li class="article">'+
+    //             '<a href="'+article.web_url+'">'+article.headline.main+
+    //                 '</a>'+
+    //                 '<p>' + article.snippet + '</p>'+
+    //             '</li>');
+    //     };
+    // });
 };
 
 $('#form-container').submit(loadData);
